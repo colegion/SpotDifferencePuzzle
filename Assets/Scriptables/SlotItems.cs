e789363d9784dc5005701b2bfb879e49aa314128
Ahmet Hakan Hafif <59022934+colegion@users.noreply.github.com>
2024-05-09 19:43:27 +0300

    Feat
    get opposite items function has been enhanced to handle more than 2 possibilities

--- Assets/Scriptables/SlotItems.cs
+++ Assets/Scriptables/SlotItems.cs
@@ -19,7 +19,29 @@ namespace Scriptables
 
         public (Item, Item) GetOppositeItems()
         {
-            return (Items[0], Items[1]);
+            List<Item> selected = new List<Item>(2);
+            
+            if (Items.Count <= 2)
+            {
+                return (Items[0], Items[1]);
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                var randomIndex = Random.Range(0, Items.Count);
+                var randomItem = Items[randomIndex];
+
+                while (selected.Contains(randomItem))
+                {
+                    randomIndex = Random.Range(0, Items.Count);
+                    randomItem = Items[randomIndex];
+                }
+        
+                selected.Add(randomItem);
+            }
+
+            return (selected[0], selected[1]);
         }
+
     }
 }
